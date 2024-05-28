8d302f84ebbe5834df4db04f646d54c4c2846eac
Rodrigo Ribeiro <rodrigogribeiro@gmail.com>
2024-05-28 11:30:15 -0300

    Little changes

--- Pcc116Code.lean
+++ Pcc116Code.lean
@@ -1,3 +1,13 @@
 -- This module serves as the root of the `Pcc116Code` library.
 -- Import modules here that should be built as part of the library.
-import «Pcc116Code».Basic
+
+import «Pcc116Code».Aula01
+import «Pcc116Code».Aula02
+import «Pcc116Code».Aula03
+import «Pcc116Code».Aula04
+import «Pcc116Code».Aula05
+import «Pcc116Code».Aula06
+import «Pcc116Code».Aula07
+import «Pcc116Code».Aula08
+import «Pcc116Code».Aula09
+import «Pcc116Code».Aula10
